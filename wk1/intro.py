e0ffb22d37fe8fe105485fff988844463e28482f
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-11 13:14:39 +0300

    day 1 discussion

---
+++ wk1/intro.py
@@ -0,0 +1,53 @@
+f_name = "Jason"
+l_name = "Brady"
+# print("hello, python", full_name)
+# print(f"my name is {f_name} {l_name}")
+is_true = False
+number = 10
+other_name = f_name
+other_name = other_name + " is cool"
+# print(f_name)
+# print (other_name)
+# List (JS Array)
+name_array = ["Jason", "Brady"]
+other_name_array = name_array
+other_name_array[0] = "Corey"
+# print(name_array)
+# print(other_name_array)
+# Dictionary (JS Object)
+python_dict = {"key": "value", "another_key": "another value"}
+# Tuple
+my_dog = ("Sheepdog", "Kai", 6)
+
+# Conditionals
+if my_dog[0] == "mutt":
+    print("Kai is a mutt!")
+else:
+    print("Kai is a pure-bred!")
+
+# Loops
+num = 5
+# for j in range(num, num-3, -1):
+#      print(j)
+
+my_list = ["Jason", "Mustafa", "Fatma", "Norah", "Somaiah", "Layla", "Dalal", "Ahad"]
+# for i in range(len(my_list)):
+#     pass
+    # print(i, my_list[i])
+
+my_dict = {"name": "Jason", "age": 41, "hair": "some", "eyes": True}
+for x, y in my_dict.items():
+    print(f"{x}: {y}")
+
+count = 0
+while count < 10:
+    count += 1
+# functions
+def add_nums(a, b):
+    return a + b
+print(add_nums(2,2))
+
+# Classes
+
+
+
