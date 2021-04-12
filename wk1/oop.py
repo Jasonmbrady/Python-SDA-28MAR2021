f1f2614f04fd8c9edb91e0832e435974bb8beb47
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-12 15:47:52 +0300

    demo complete

--- wk1/oop.py
+++ wk1/oop.py
@@ -12,15 +12,45 @@ class Ninja:
         target.health -= self.strength
         print(f"{self.name} attacks {target.name} and deals {self.strength} damage!")
         print(target.name, "has", target.health, "hp left!")
+        print(self.name, "has", self.health, "hp left!")
         return self
 
+# Inheritance
+class Tiger_Clan(Ninja):
+    def __init__(self, name, health, strength):
+        super().__init__(name, health, strength)
+        self.is_crouching = True
 
+    def attack(self, target):
+        if self.is_crouching:
+            self.strength += self.strength
+            self.is_crouching = False
+            super().attack(target)
+        else:
+            self.is_crouching = True
+            self.strength -= self.strength//2
+            super().attack(target)
+        return self
 
-# Leo = Ninja("Leo", 20)
-# Raph = Ninja("Raph")
+class Dragon_Clan(Ninja):
+    def __init__(self, name, health, strength):
+        super().__init__(name, health, strength)
+        self.is_hidden = True
 
-# Leo.attack(Raph).check_health()
-# Leo.check_health()
-# Raph.check_health()
+    def attack(self, target):
+        if self.is_hidden:
+            self.health += self.strength
+            self.is_hidden = False
+            super().attack(target)
+        else:
+            self.is_hidden = True
+            super().attack(target)
+        return self
 
+Leo = Tiger_Clan("Leo", 100, 10)
+Raph = Dragon_Clan("Raph", 100, 10)
 
+Leo.attack(Raph)
+Raph.attack(Leo)
+Leo.attack(Raph)
+Raph.attack(Leo)
